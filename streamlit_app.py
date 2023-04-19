c943c7dfd2f6df759376e21a18ec8145ba3e6472
Prakriti-nucleusteq <109510546+Prakriti-nucleusteq@users.noreply.github.com>
2023-04-19 16:34:35 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+streamlit.title('Omega 3 & Blueberry Datmeal')
+streamlit.title('kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
