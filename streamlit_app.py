586cf976b9329b3a9c1f6c4f5e65373c9e33d9cc
Prakriti-nucleusteq <109510546+Prakriti-nucleusteq@users.noreply.github.com>
2023-04-24 15:45:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import Snowflake.connector
+
 
 streamlit.title('Breakfast favroities')
 streamlit.header('Breakfast favroities')
@@ -27,4 +29,3 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-import Snowflake.connector
